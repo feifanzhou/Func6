ca9794bb77d37207cf323d1116b26f955fed88e3
Feifan Zhou <feifan@me.com>
2014-04-28 21:27:22 -0400

    Handle initial move (assuming valid)

--- release/game.ml
+++ release/game.ml
@@ -17,7 +17,12 @@ let handle_move s m = let (board, player_list, turn, (color, curr_req)) = s in
   match curr_req with
   | InitialRequest ->
     match m with 
-    | InitialMove (p1, p2) ->
+    | InitialMove (p1, p2) -> 
+      let town = (color, Town) in (* TODO: Check town location *)
+      let road = (color, (p1, p2)) in
+      let (map, (curr_intrs, curr_roads), deck, disc, robber) = board in
+      let new_board = (map, ((town::curr_intrs), (road::curr_roads)), deck, disc, robber) in
+      (None, (new_board, player_list, turn, (color, curr_req)))
     | _ -> failwith "Fill in valid moves"
   | RobberRequest ->
     match m with
@@ -35,15 +40,18 @@ let handle_move s m = let (board, player_list, turn, (color, curr_req)) = s in
     | Action a ->
       match a with
       | RollDice -> let roll_num = Util.random_roll () in
-        let new_turn = {  (* Update turn with roll number *)
-          active = turn.active;
-          dicerolled = Some roll_num;
-          cardplayed = turn.cardplayed;
-          cardsbought = turn.cardsbought;
-          tradesmade = turn.tradesmade;
-          pendingtrade = turn.pendingtrade
-        } in
-        (None, (board, player_list, new_turn, (color, curr_req)))
+        if roll_num = cROLL_ROBBER
+        then (None, s) (* Do nothing for now *)
+        else 
+          let new_turn = {  (* Update turn with roll number *)
+            active = turn.active;
+            dicerolled = Some roll_num;
+            cardplayed = turn.cardplayed;
+            cardsbought = turn.cardsbought;
+            tradesmade = turn.tradesmade;
+            pendingtrade = turn.pendingtrade
+          } in (* TODO: Do resources get generated as part of this move or the next one? *)
+          (None, (board, player_list, new_turn, (color, curr_req)))
       | MaritimeTrade (resource1, resource2) -> failwith "Not yet"
       | DomesticTrade (color, cost1, cost2) -> failwith "Not yet"
       | BuyBuild b -> failwith "Not yet"
